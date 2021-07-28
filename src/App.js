e87297f8207c568c51ace6433a5ea9e56aac83a8
h-sompura <sompharshi123@gmail.com>
2021-07-28 23:36:38 +0530

    Added theme and global styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-
+import { ThemeProvider,createGlobalStyle } from "styled-components";
+import Theme from "./styled/Theme";
 
 function App() {
+  const GlobalStyles = createGlobalStyle`
+    *{
+      box-sizing:border-box;
+      margin:0;
+      padding:0;
+    }
+    body,html {
+      height:100%;
+      background-color: ${props => props.theme.colors.mainBackground};
+      color: ${props => props.theme.colors.textMain};
+    }
+  `;
   return (
+    <ThemeProvider theme={Theme}>
     <div className="App">
+      <GlobalStyles />
       <p>Hello!</p>
     </div>
+    </ThemeProvider>
   );
 }
 
---
+++ src/styled/Theme.js
@@ -0,0 +1,13 @@
+const theme = {
+    colors: {
+        mainBackground: "#16161A",
+        secondaryBackground: "#72757E",
+        textMain: "#FFFFFE",
+        textParagraph: "#94A1B2",
+        primaryColor: "#7F5AF0",
+        secondaryColor: "#2CB67D",
+    },
+    textSize: ["24px", "18px","14px"],
+
+}
+export default theme;
