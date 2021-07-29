127b75d51a445c167f3632497f1ae5eb97e449af
h-sompura <sompharshi123@gmail.com>
2021-07-29 12:47:47 +0530

    Added react-icons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import { useContext } from 'react';
 import logo from "../assets/Logo.png";
-import { NavWrapper, NavLogo, NavLink } from '../styled';
+import { NavWrapper, NavLogo, NavLink, NavCode } from '../styled';
+import {AiFillGithub} from 'react-icons/ai';
+import { ThemeContext } from 'styled-components';
 export const Navbar = () => {
+    const themeContext = useContext(ThemeContext);
     return(
         <NavWrapper>
             <NavLogo src={logo} />
             <NavLink to='/'>Reusable Variants</NavLink>
-            <NavLink to='/'>View code</NavLink>
+            <AiFillGithub style={{ color: themeContext.colors.textParagraph}} size={20} />
+            <NavCode target="_blank" href="https://github.com/h-sompura/reusable-variants.git">View code</NavCode>
         </NavWrapper>
     )
 }
--- src/styled/NavWrapper.js
+++ src/styled/NavWrapper.js
@@ -8,11 +8,25 @@ export const NavLogo =styled.img`
     height:1.3em;
 `;
 export const NavLink = styled(Link)`
+    justify-content:flex-start;
     color: ${props => props.theme.colors.textMain};
     font-size: ${props => props.theme.textSize.normal};
     margin:0 8px;
+    flex-grow:1;
     text-decoration:none;
     transition:100ms;
+    &:hover{
+        font-weight:bold;
+        text-decoration:underline;
+        color: ${props => props.theme.colors.secondaryColor};
+    }
+`;
+export const NavCode = styled.a`
+    color: ${props => props.theme.colors.textMain};
+    font-size: ${props => props.theme.textSize.normal};
+    text-decoration:none;
+    transition:100ms;
+    margin-left:8px;
     &:hover{
         font-weight:bold;
         text-decoration:underline;
